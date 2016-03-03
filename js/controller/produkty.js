0527938ef5cfb26f65ad06a6f7c7333ed55094ed
Rjakubiec <jakubiecrafal@gmail.com>
2016-03-03 18:27:01 +0100

    poprawkiCD

--- js/controller/produkty.js
+++ js/controller/produkty.js
@@ -82,6 +82,8 @@ angular.module('szczesniak')
             $scope.filter1 = 0;
             $scope.filter2 = 0;
             $scope.filter3 = 0;
+            $location.path('products');
+            
         };
         $scope.reset2 = function () {
 
@@ -91,4 +93,29 @@ angular.module('szczesniak')
 
 
 
-    });
+    }).filter('spaceless',function() {
+    return function(input) {
+        if (input) {
+            input=input.replace(/\s+/g, '_');
+            input=input.replace(/ą/ig, 'a');
+            input=input.replace(/Ą/ig, 'A');
+            input=input.replace(/Ę/ig, 'E');
+            input=input.replace(/ę/ig, 'e');
+            input=input.replace(/ć/ig, 'c');
+            input=input.replace(/Ć/ig, 'C');
+            input=input.replace(/Ś/ig, 'S');
+            input=input.replace(/ś/ig, 's');
+            input=input.replace(/ł/ig, 'l');
+            input=input.replace(/Ł/ig, 'L');
+            input=input.replace(/Ń/ig, 'Ń');
+            input=input.replace(/ń/ig, 'n');
+            input=input.replace(/Ó/ig, 'O');
+            input=input.replace(/ó/ig, 'o');
+            input=input.replace(/Ż/ig, 'Z');
+            input=input.replace(/ż/ig, 'z');
+            input=input.replace(/ź/ig, 'z');
+            input=input.replace(/Ź/ig, 'Z');
+            
+            return input;     
+        }
+    }});;
